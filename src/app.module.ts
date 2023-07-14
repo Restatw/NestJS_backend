4580805a8e5e6c2a7ac1ea33d5252dfa0601c3d6
restatw <xz84121a@gmail.com>
2023-07-14 22:55:16 +0800

    add sqlite method

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,13 +15,19 @@ const isProd = process.env.NODE_ENV == "production";
     ConfigModule.forRoot({
       envFilePath: [ isProd ? path.resolve(".env.prod") : path.resolve(".env") ]
     }),
+    // TypeOrmModule.forRoot({
+    //   type: "mysql",
+    //   autoLoadEntities: true,
+    //   host: "localhost",
+    //   username: "root",
+    //   password: "root",
+    //   database: "default",
+    //   synchronize: true,
+    // }),
     TypeOrmModule.forRoot({
-      type: "mysql",
+      type: 'sqlite',
+      database: 'db.sql',
       autoLoadEntities: true,
-      host: "localhost",
-      username: "root",
-      password: "root",
-      database: "default",
       synchronize: true,
     }),
     UserModule,
